584060ef5c8ffa7c2aa38d3a2603258f5b93d70d
pedropsb <pedropsb95@gmail.com>
2023-10-10 16:01:58 -0300

    add merge-case data class to organize tests

--- dynamerge/merge_cases.py
+++ dynamerge/merge_cases.py
@@ -1,59 +1,72 @@
 from dynamerge.merger import Merger
 from icecream import ic
+from functools import wraps
+
+from dataclasses import dataclass
+from typing import Callable
 
 
-def merge_report(name: str, old, new, merge_fn, **kwargs):
-    print("\n> ", name)
-    old = {"root": old}
-    new = {"root": new}
-    ic(old)
-    ic(new)
-    out = Merger.merge_dict(old, new, **kwargs)
-    ic(out)
+@dataclass
+class MergeCase:
+    name: str
+    old: dict | list
+    new: dict | list
+    expected: dict | list
 
 
 def main():
+    for case in merge_cases():
+        merge_report(case)
     # case_merge_dicts()
-    case_merge_false()
+    # case_merge_false()
 
 
-def case_merge_false():
-    merge_report(
-        "merge_false: root_dict",
-        {"a": "A", "b": "B"},
-        {"a": "A", "dynaconf_merge": False},
-        Merger.merge_dict,
-    )
-
-    # merge_report(
-    #     "merge_false: nested",
-    #     {"a": "A", "b": {"c": "C", "d": {"e": "E", "f": {"g": "G", "h": "H"}}}},
-    #     {"b": {"d": {"f": {"g": "G*"}}}, "dynaconf_merge": False},
-    #     Merger.merge_dict,
-    # )
+def merge_report(case: MergeCase):
+    name = case.name
+    old = {"r": case.old}
+    new = {"r": case.new}
+    exp = {"r": case.expected}
+    print("\n> ", name)
+    ic(old)
+    ic(new)
+    out = Merger.merge_dict(old, new)
+    ic(out)
+    ic(exp)
 
 
-def case_merge_dicts():
-    merge_report(
-        "simple merge",
-        {"a": "A", "b": "B"},
-        {"a": "A*", "c": "C"},
-        Merger.merge_dict,
-    )
-
-    merge_report(
-        "nested dicts: new overrides common keys",
-        {"a": "A", "b": {"c": "C", "d": "D"}},
-        {"a": "A*", "b": {"c": "C*", "e": "E"}},
-        Merger.merge_dict,
-    )
-
-    merge_report(
-        "nested dicts: new merges different key",
-        {"a": "A", "b": {"c": "C", "d": {"e": "E", "f": {"g": "G", "h": "H"}}}},
-        {"b": {"d": {"f": {"g": "G*"}}}},
-        Merger.merge_dict,
-    )
+def merge_cases():
+    return [
+        MergeCase(
+            "un-nested dicts",
+            {"a": "A", "b": "B"},
+            {"a": "A*", "c": "C"},
+            {"a": "A*", "b": "B", "c": "C"},
+        ),
+        MergeCase(
+            "nested dicts: new-old overrides common keys",
+            {"a": "A", "b": {"c": "C", "d": "D"}},
+            {"a": "A*", "b": {"c": "C*", "e": "E"}},
+            {"a": "A*", "b": {"c": "C*", "d": "D", "e": "E"}},
+        ),
+        MergeCase(
+            "nested dicts: new-only merges different key",
+            {"a": "A", "b": {"c": "C", "d": {"e": "E", "f": {"g": "G", "h": "H"}}}},
+            {"b": {"d": {"f": {"g": "G*"}}}},
+            {"a": "A", "b": {"c": "C", "d": {"e": "E", "f": {"g": "G*", "h": "H"}}}},
+        ),
+        MergeCase(
+            "merge_false: simple",
+            {"a": "A", "b": "B"},
+            {"a": "A*", "dynaconf_merge": False},
+            {"a": "A*"},
+        ),
+        MergeCase(
+            "merge_false: nested",
+            {"a": "A", "b": {"c": "C", "d": {"e": "E", "f": {"g": "G", "h": "H"}}}},
+            {"b": {"d": {"f": {"g": "G*"}}}, "dynaconf_merge": False},
+            {"b": {"d": {"f": {"g": "G*"}}}},
+        ),
+    ]
 
 
 if __name__ == "__main__":
